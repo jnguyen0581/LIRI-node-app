ffac96ac36964c0bf8fbace3e24290edb0bdb62a
JoyceNguyen <jnguyen0581@gmail.com>
2018-11-03 23:24:21 -0500

    added .gitignore, package.json and package-lock.json

--- liri.js
+++ liri.js
@@ -1,1 +1,27 @@
 require("dotenv").config();
+
+// assumes user setup has been completed (api keys...)
+
+// get the user input
+const input = process.argv[2];
+//  make a decision based on the command
+switch (input) {
+    case "concert-this":
+        concertThis();
+        break;
+
+    case "spotify-this-song":
+        spotifyThisSong();
+        break;
+    default:
+        console.log("I don't understand, ask Google");
+        break;
+}
+
+function spotifyThisSong(){
+    console.log("SPOTIFY THIS SONG")
+}
+
+function concertThis() {
+console.log("CONCERT THIS");
+}
